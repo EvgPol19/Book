dfc408dc8ebcd6f94948e56b31e2046643f4edd4
EvgPol19 <elulbako@list.ru>
2021-05-23 02:11:06 +0300

    1

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from .local_settings import local_settings
+from .local_settings import ALLOWED_HOSTS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
